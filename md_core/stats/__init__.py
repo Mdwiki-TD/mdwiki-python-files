72812384bd51f062a20bb3519e966e4af3fff2c7
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-01-13 23:59:10 +0300

    1

--- md_core/stats/__init__.py
+++ md_core/stats/__init__.py
@@ -5,6 +5,10 @@ python3 core8/pwb.py stats/qids
 python3 core8/pwb.py stats/sitelinks
 
 python3 core8/pwb.py stats/o
-python3 core8/pwb.py stats/o site:ar
+
+python3 core8/pwb.py stats/o dump
+
+tfj run statsdump --image python3.9 --command "$HOME/local/bin/python3 core8/pwb.py stats/o dump"
+
 
 '''
