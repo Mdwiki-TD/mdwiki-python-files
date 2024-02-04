3101583433bbe68e399dce7ed59008a31143aca6
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-02-04 23:49:12 +0300

    .

--- md_core/after_translate/sql_new.py
+++ md_core/after_translate/sql_new.py
@@ -2,7 +2,6 @@
 """
 بوت قواعد البيانات
 
-python3 core8/pwb.py after_translate/sql justsql break
 python3 core8/pwb.py after_translate/sql justsql
 python3 core8/pwb.py after_translate/sql
 
@@ -20,6 +19,7 @@ import time
 from newapi import printe
 
 # ---
+from after_translate.bots.get_pages import get_pages_from_db
 from after_translate.bots import add_to_wd
 from after_translate.bots.add_to_mdwiki import add_to_mdwiki_sql
 from after_translate.bots.fixcat import cat_for_pages
@@ -28,10 +28,6 @@ from after_translate.bots.fixcat import cat_for_pages
 from mdpy.bots import py_tools
 from mdpy.bots import sql_for_mdwiki
 from api_sql import wiki_sql
-# ---
-targets_done = {}
-langs_to_t_u = {}
-to_update = {}
 # ---
 skip_langs = ["zh-yue", "ceb"]
 # ---
@@ -67,26 +63,6 @@ Skip_titles_global = [
     "موميتازون",
 ]
 # ---
-tit_user_lang = {}
-# ---
-query_main_old = """
-    select DISTINCT p.page_title, c.comment_text , a.actor_name , r.rev_timestamp
-    from change_tag t
-    INNER JOIN change_tag_def ctd on ctd.ctd_id = t.ct_tag_id
-    INNER JOIN revision r on r.rev_id = t.ct_rev_id
-    INNER JOIN actor a ON r.rev_actor = a.actor_id
-    inner join comment c on c.comment_id = r.rev_comment_id
-    INNER JOIN page p on r.rev_page=p.page_id
-    where ctd.ctd_name in ("contenttranslation", "contenttranslation-v2") #id = 3 # id = 120
-    and r.rev_parent_id = 0
-    #AND a.actor_name in ('Mr. Ibrahem')
-    AND r.rev_timestamp > 20210101000000
-    #and comment_text like "%[[:en:Special:Redirect/revision/%|User:Mr. Ibrahem/%]]%"
-    and comment_text like "%User:Mr. Ibrahem/%"
-    and p.page_namespace = 0
-    #limit 10
-"""
-# ---
 query_main = """
     select DISTINCT p.page_title as title,
     SUBSTRING_INDEX(SUBSTRING_INDEX(c.comment_text, 'Ibrahem/', -1), ']]', 1) as comment_text,
@@ -99,104 +75,25 @@ query_main = """
     INNER JOIN page p on r.rev_page=p.page_id
     where ctd.ctd_name in ("contenttranslation", "contenttranslation-v2") #id = 3 # id = 120
     #and r.rev_parent_id = 0
-    AND r.rev_timestamp > 20210101000000
+    # AND r.rev_timestamp > 20210101000000
+    AND r.rev_timestamp > 20240101000000
     and comment_text like "%User:Mr. Ibrahem/%"
     #and p.page_namespace = 0
     group by p.page_title, a.actor_name, c.comment_text
 """
 
-
-def get_pages_from_db():
-    # ---
-    lang_o = ""
-    # ---
-    for arg in sys.argv:
-        arg, _, value = arg.partition(":")
-        if arg in ["lang", "-lang"]:
-            lang_o = value
-            langs_to_t_u[value] = {}
-        # ---
-    # ---
-    que = "select title, user, lang, target from pages "
-    # ---
-    if lang_o != "":
-        que += f' where lang = "{lang_o}"'
-    # ---
-    que += " ;"
-    # ---
-    printe.output(que)
-    # ---
-    sq = sql_for_mdwiki.mdwiki_sql(que, return_dict=True)
-    # ---
-    len_no_target = 0
-    len_done_target = 0
-    # ---
-    for tab in sq:
-        mdtitle = tab["title"]
-        user = tab["user"]
-        target = tab["target"]
-        lang = tab["lang"].lower()
-        # ---
-        if lang_o != "" and lang != lang_o.strip():
-            continue
-        # ---
-        tul = mdtitle + user + lang
-        tit_user_lang[tul] = target
-        # ---
-        if lang not in langs_to_t_u:
-            langs_to_t_u[lang] = {}
-        # ---
-        if lang not in to_update:
-            to_update[lang] = {}
-        # ---
-        if user not in to_update[lang]:
-            to_update[lang][user] = []
-        # ---
-        if target == "":
-            len_no_target += 1
-            # ---
-            langs_to_t_u[lang][mdtitle] = user
-            # ---
-            to_update[lang][user].append(mdtitle)
-            # ---
-        else:
-            # ---
-            if lang not in targets_done:
-                targets_done[lang] = {}
-            # ---
-            target = target.replace("_", " ")
-            target2 = py_tools.ec_de_code(target, "encode")
-            # ---
-            len_done_target += 1
-            # ---
-            targets_done[lang][target] = {"user": user, "target": target}
-            targets_done[lang][target2] = {"user": user, "target": target}
-    # ---
-    printe.output(f"<<lightyellow>> find {len_done_target} with target, and {len_no_target} without in mdwiki database. ")
-    # ---
-    if "print" in sys.argv:
-        printe.output(langs_to_t_u)
-    # ---
-    time.sleep(3)
-
-
-def start(result, lange):
+def start(result, lange, tgd, tit_user_lang):
     printe.output(f'sql.py len(result) = "{len( result )}"')
     # ---
-    # texddt = '\n'
-    # ---
     for lis in result:
-        # ---
-        # printe.output( lis )
         # ---
         target = lis["title"]
         co_text = lis["comment_text"]
         user = lis["actor_name"]
         pupdate = lis["rev_timestamp"]
-        namespace = lis["page_namespace"]
-        # rev_parent_id = lis['rev_parent_id']
+        ns = lis["page_namespace"]
         # ---
-        namespace = str(namespace)
+        ns = str(ns)
         pupdate = pupdate[:8]
         pupdate = re.sub(r"^(\d\d\d\d)(\d\d)(\d\d)$", r"\g<1>-\g<2>-\g<3>", pupdate)
         # ---
@@ -221,12 +118,10 @@ def start(result, lange):
             "user": user,
             "lang": lange,
             "pupdate": pupdate,
-            "namespace": namespace,
+            "namespace": ns,
         }
         # ---
-        laox = f"<<lightyellow>> target:{lange}:{target.ljust(40)}, ns:{namespace.ljust(3)} for mdtit:<<lightyellow>>{md_title.ljust(30)}, user:<<lightyellow>>{user}"
-        # ---
-        tgd = targets_done.get(lange, {})
+        laox = f"<<lightyellow>> target:{lange}:{target.ljust(40)}, ns:{ns.ljust(3)} for mdtit:<<lightyellow>>{md_title.ljust(30)}, user:<<lightyellow>>{user}"
         # ---
         target2 = py_tools.ec_de_code(target, "encode")
         # ---
@@ -235,7 +130,7 @@ def start(result, lange):
         # ---
         cattest = cat_for_pages.get(md_title, "")
         # ---
-        if namespace != "0":
+        if ns != "0":
             if "ns" in sys.argv and tul_target == "" and cattest:
                 printe.output(laox)
             continue
@@ -249,14 +144,22 @@ def start(result, lange):
             elif tul_target == target:
                 printe.output(f"target already in, {target}")
             else:
-                printe.output(
-                    f"puplished target: {tul_target} != target to add: {target}"
-                )
+                printe.output(f"puplished target: {tul_target} != target to add: {target}")
 
 
 def main():
     # ---
-    get_pages_from_db()
+    lang_o = ""
+    # ---
+    for arg in sys.argv:
+        arg, _, value = arg.partition(":")
+        if arg in ["lang", "-lang"]:
+            lang_o = value
+        # ---
+    # ---
+    to_update, langs_to_t_u, targets_done, tit_user_lang = get_pages_from_db(lang_o)
+    # ---
+    time.sleep(3)
     # ---
     numb_lang = 0
     lnn = len(langs_to_t_u.keys())
@@ -294,8 +197,10 @@ def main():
         # ---
         result = wiki_sql.sql_new(qua, str(lange))
         # ---
+        tgd = targets_done.get(lange, {})
+        # ---
         if result != {}:
-            start(result, lange)
+            start(result, lange, tgd, tit_user_lang)
         # ---
         add_to_wd.add_tab_to_wd({lange: tab_by_lang[lange]})
         # ---
