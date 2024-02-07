abb435bc712313eed4f6137b864c2f40a543618d
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-02-08 01:25:29 +0300

    Update unlinkedwikibase.py

--- md_core/mdpages/qids_others/unlinkedwikibase.py
+++ md_core/mdpages/qids_others/unlinkedwikibase.py
@@ -3,25 +3,26 @@
 This script performs various operations related to MediaWiki pages and Wikibase QIDs.
 It includes functions for filtering and processing page titles, checking page existence, 
 retrieving and modifying page content, and adding tags based on QIDs.
+
+Usage:
+python3 core8/pwb.py mdpages/qids_others/unlinkedwikibase add
 """
-# python3 core8/pwb.py mdpages/qids_others/unlinkedwikibase
 # ---
 import sys
 from mdpy.bots import sql_for_mdwiki
-from mdpages.qids_others import sql_qids_others
-from mdpy.bots import mdwiki_api
+from mdpy.bots.check_title import valid_title
 from mdpy import printe
-from mdpy.bots.check_title import valid_title  # valid_title(title)
-
+from mdpages.qids_others import sql_qids_others
 # ---
-from newapi.mdwiki_page import MainPage as md_MainPage
-
+from newapi.mdwiki_page import NEW_API, MainPage as md_MainPage
+api_new = NEW_API('www', family='mdwiki')
+api_new.Login_to_wiki()
 # ---
 qids1 = sql_for_mdwiki.get_all_qids()
 qids2 = sql_qids_others.get_others_qids()
 # ---
-qids1 = {x: v for x, v in qids1.items() if v != ''}
-qids2 = {x: v for x, v in qids2.items() if v != ''}
+qids1 = {x: v for x, v in qids1.items() if v != ""}
+qids2 = {x: v for x, v in qids2.items() if v != ""}
 # ---
 vals_d = {}
 # ---
@@ -36,12 +37,12 @@ qids = {v[0]: q for q, v in vals_d.items() if len(v) == 1}
 # ---
 for q, v in vals_d.items():
     if len(v) > 1:
-        printe.output(f'<<red>> duplicate: q:{q}, v:{v}')
+        printe.output(f"<<red>> duplicate: q:{q}, v:{v}")
 
 
 def work_page(title, qid):
     # ---
-    page = md_MainPage(title, 'www', family='mdwiki')
+    page = md_MainPage(title, "www", family="mdwiki")
     exists = page.exists()
     if not exists:
         return
@@ -53,38 +54,42 @@ def work_page(title, qid):
     # refs        = page.Get_tags(tag='ref')# for x in ref: name, contents = x.name, x.contents
     # templates = page.get_templates()
     # ---
-    if text.find('{{#unlinkedwikibase:id=') != -1:
-        printe.output('page already tagged')
+    if text.find("{{#unlinkedwikibase:id=") != -1:
+        printe.output("page already tagged")
         return
     # ---
-    tag = '{{#unlinkedwikibase:id=' + qid + '}}\n'
+    tag = "{{#unlinkedwikibase:id=" + qid + "}}\n"
     # ---
     newtext = tag + text.strip()
-    page.save(newtext=newtext, summary='add tag:' + tag, nocreate=1, minor='')
+    page.save(newtext=newtext, summary="add tag:" + tag, nocreate=1, minor="")
 
 
 def add_tag():
     # ---
-    printe.output('Get all pages...')
+    printe.output("Get all pages...")
     # ---
-    all_pages = mdwiki_api.Get_All_pages('!', namespace='0', apfilterredir='nonredirects')
+    all_pages = api_new.Get_All_pages(start='!', namespace="0", apfilterredir='nonredirects')
     # ---
     all_pages = [x for x in all_pages if valid_title(x)]
-    printe.output(f'len of all_pages: {len(all_pages)}')
+    # ---
+    printe.output(f"len of all_pages: {len(all_pages)}")
+    # ---
+    pages_has = api_new.Search(value='{{#unlinkedwikibase:id=', ns="0")
+    # ---
+    printe.output(f"len of pages_has: {len(pages_has)}")
     # ---
     for n, x in enumerate(all_pages):
-        printe.output(f'p:{n}/{len(all_pages)}: t:{x}::')
+        printe.output(f"p:{n}/{len(all_pages)}: t:{x}::")
         # ---
-        # qid = qids1.get(x) or qids2.get(x)
         qid = qids.get(x)
         # ---
         if not qid:
-            printe.output('no qid')
+            printe.output("no qid")
             continue
         # ---
         work_page(x, qid)
 
 
-if __name__ == '__main__':
-    if 'add' in sys.argv:
+if __name__ == "__main__":
+    if "add" in sys.argv:
         add_tag()
