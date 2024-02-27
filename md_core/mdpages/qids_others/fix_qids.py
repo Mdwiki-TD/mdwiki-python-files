d52e6d3abebcf15f356e9cad85520a88c2d26b04
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-02-28 02:36:33 +0300

    Update fix_qids.py

--- md_core/mdpages/qids_others/fix_qids.py
+++ md_core/mdpages/qids_others/fix_qids.py
@@ -11,10 +11,22 @@ from mdpy.bots import wikidataapi
 from mdpy import printe
 from mdpy.bots import sql_for_mdwiki
 from mdpages.qids_others import sql_qids_others
+from mdpages.qids_others.unlinkedwikibase import work_page#(title, qid)
 # ---
 qids_others = sql_qids_others.get_others_qids()
 # ---
+qids_to_title = { q : t for t, q in qids_others.items() }
+# ---
 to_work = [q for q in qids_others.values() if q != '']
+
+def work_un(tab):
+    for numb, (old_q, new_q) in enumerate(reds.items(), start=1):
+        # ---
+        title = qids_to_title.get(old_q)
+        printe.output(f'<<lightblue>> {numb}, {title=}, {old_q=}, {new_q=}')
+        # ---
+        work_page(title, new_q)
+
 
 def fix_redirects():
     # ---
@@ -38,6 +50,8 @@ def fix_redirects():
         else:
             printe.output(qua)
             printe.output('<<lightgreen>> add "fix" to sys.argv to fix them..')
-
+    # ----
+    work_un(reds)
+    
 if __name__ == '__main__':
     fix_redirects()
