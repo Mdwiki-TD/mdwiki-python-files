77528d7e7430a473e148276f96de352191e89dc4
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-03-02 23:28:35 +0300

    Update and rename count1.py to count.py

--- mass/radio/st3/count.py
+++ mass/radio/st3/count.py
@@ -8,6 +8,7 @@ import json
 import tqdm
 from pathlib import Path
 from mass.radio.studies import get_images_stacks, get_images
+from newapi.ncc_page import MainPage as ncc_MainPage
 
 main_dir = Path(__file__).parent.parent
 
@@ -20,8 +21,11 @@ with open(main_dir / "jsons/cases_in_ids.json", "r", encoding="utf-8") as f:
 ids_tab = {x: v for x, v in ids.items() if x not in cases_in_ids}
 
 class All:
+    cases = 0
     images = 0
     studies = 0
+    
+All.cases = len(ids_tab)
 
 def get_studies(studies_ids, caseId):
     images_count = 0
@@ -41,18 +45,36 @@ def get_studies(studies_ids, caseId):
             url = f"https://radiopaedia.org/cases/{caseId}/studies/{study}"
             images = get_images(url)
         images_count += len(images)
+
     return images_count
 
+def sa():
+    text = f"* Cases: {All.cases:,}\n"
+    text = f"* Images: {All.images:,}\n"
+    text = f"* Studies: {All.studies:,}\n"
+
+    print(text)
+
+    page = ncc_MainPage("User:Mr. Ibrahem/Radiopaedia", 'www', family='nccommons')
+
+    if page.exists():
+        page.save(newtext=text, summary='update')
+    else:
+        page.Create(text=text, summary='update')
+    
 def start():
     print(f"<<purple>> start.py all: {len(ids_tab)}:")
+
     for _, va in tqdm.tqdm(ids_tab.items()):
         caseId = va["caseId"]
+
         studies = [study.split("/")[-1] for study in va["studies"]]
         All.studies += len(studies)
+
         images = get_studies(studies, caseId)
         All.images += images
-    print(f"{All.images=}")
-    print(f"{All.studies=}")
+        
+    sa()
 
 if __name__ == '__main__':
     start()
