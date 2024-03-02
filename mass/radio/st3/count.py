46885fa70cebb7107747189512bc181ef12d6ddc
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-03-02 23:45:10 +0300

    Update count.py

--- mass/radio/st3/count.py
+++ mass/radio/st3/count.py
@@ -1,7 +1,7 @@
 """
 
 python3 core8/pwb.py mass/radio/st3/count
-
+tfj run countca --image python3.9 --command "$HOME/local/bin/python3 core8/pwb.py mass/radio/st3/count"
 """
 import os
 import sys
