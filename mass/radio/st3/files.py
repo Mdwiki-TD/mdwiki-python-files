8f4e96b69cf921c2967b52a24144b8d7a2fc36a3
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-03-09 00:02:16 +0300

    Create files.py

---
+++ mass/radio/st3/files.py
@@ -0,0 +1,159 @@
+'''
+
+python3 core8/pwb.py mass/radio/authors_list/auths_cats
+
+tfj run aucts --image python3.9 --command "$HOME/local/bin/python3 core8/pwb.py mass/radio/authors_list/auths_cats"
+
+logs in page:
+https://nccommons.org/wiki/User:Mr._Ibrahem/Radiopaedia_authors
+
+'''
+import re
+import sys
+import json
+import os
+from pathlib import Path
+from multiprocessing import Pool
+from newapi import printe
+from newapi.ncc_page import CatDepth
+from newapi.ncc_page import MainPage as ncc_MainPage
+
+def cases_cats():
+    members = CatDepth('Category:Radiopaedia images by case', sitecode='www', family="nccommons", depth=0, ns="14")
+    reg = r'^Category:Radiopaedia case (\d+) (.*?)$'
+    # ---
+    id2cat = {}
+    # ---
+    for cat in members:
+        match = re.match(reg, cat)
+        if match:
+            case_id = match.group(1)
+            # ---
+            id2cat[case_id] = cat
+    # ---
+    print(f'lenth of members: {len(members)} ')
+    print(f'lenth of id2cat: {len(id2cat)} ')
+
+    return id2cat
+
+def images_to_cats():
+    members = CatDepth('Category:Radiopaedia_images_by_system', sitecode='www', family="nccommons", depth=1, ns="10")
+    reg = r'^File:.*? \(Radiopaedia (\d+)\)\.\w+$'
+    # ---
+    tab = {}
+    # ---
+    for file in members:
+        match = re.match(reg, file)
+        if match:
+            case_id = match.group(1)
+            # ---
+            tab[file] = case_id
+    # ---
+    print(f'images_to_cats, lenth of members: {len(members)} ')
+    print(f'images_to_cats, lenth of id2cat: {len(id2cat)} ')
+
+    return tab
+
+
+def create_cat(cat, text):
+    page = ncc_MainPage(cat, 'www', family='nccommons')
+
+    if page.exists():
+        pa_text = page.get_text()
+        if pa_text == text:
+            print("no different")
+            return
+        page.save(newtext=text, summary='create')
+    else:
+        page.Create(text=text, summary='create')
+
+
+def add(da=[], title="", cat=""):
+    if da:
+        title, cat = da[0], da[1]
+    # ---
+    page = ncc_MainPage(title, 'www', family='nccommons')
+
+    if not page.exists():
+        return
+
+    text = page.get_text()
+    # ---
+    if text.find(cat) != -1:
+        printe.output(f"cat {title} already has it.")
+        return
+    # ---
+    newtext = text
+    newtext += f"\n[[{cat}]]"
+    # ---
+    page.save(newtext=newtext, summary=f'Bot: added [[:{cat}]]')
+
+
+def mu(tab):
+    pool = Pool(processes=3)
+    pool.map(add, tab)
+    pool.close()
+    pool.terminate()
+
+
+def add_cat(pages, cat):
+    if "multi" in sys.argv:
+        tab = [[x, cat] for x in pages]
+        mu(tab)
+    else:
+        for title in pages:
+            add(title=title, cat=cat)
+
+
+def one_auth(auth, cat_list):
+    printe.output(f"Author: {auth}, {len(cat_list)=}")
+    # ---
+    cat  = f"Category:Radiopaedia cases by {auth}"
+    text = ""
+    # ---
+    url      = authors_infos.get(auth, {}).get('url')
+    location = authors_infos.get(auth, {}).get('location')
+    # ---
+    if url:
+        text += f"* Author: [{url} {auth}]\n"
+    else:
+        text += f"* Author: {auth}\n"
+    # ---
+    text += f"[[Category:Radiopaedia cases by author|{auth}]]"
+    # ---
+    create_cat(cat, text)
+    # ---
+    done = CatDepth(cat, sitecode='www', family="nccommons", depth=0, ns="14")
+    # ---
+    new_cat_list = [x for x in cat_list if x not in done]
+    # ---
+    printe.output(f"{len(done)=}, {len(new_cat_list)=}")
+    # ---
+    if "noadd" not in sys.argv:
+        add_cat(new_cat_list, cat)
+
+
+def start():
+    # ---
+    cats = cases_cats()
+    # ---
+    # auths in authors_to_cases with > 10 cases
+    authors_cases = {k: v for k, v in authors_to_cases.items() if len(v) > 1}
+    # ---
+    for numb, (x, x_cases) in enumerate(authors_cases.items(), start=1):
+        # ---
+        printe.output(f"{x=}, cases: {len(x_cases)=}")
+        # ---
+        cat_list = [cats[c] for c in x_cases if c in cats]
+        cat_no_list = [c for c in x_cases if c not in cats]
+        # ---
+        printe.output(f"<<red>> {len(cat_no_list)=}")
+        # ---
+        one_auth(x, cat_list)
+        # ---
+        if "break" in sys.argv and numb % 10 == 0:
+            break
+
+
+if __name__ == '__main__':
+    start()
