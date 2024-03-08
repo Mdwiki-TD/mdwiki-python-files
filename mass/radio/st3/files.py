21c2c86e7774e7cabd94c507fb97256ab6f9d795
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-03-09 00:13:16 +0300

    Update files.py

--- mass/radio/st3/files.py
+++ mass/radio/st3/files.py
@@ -77,7 +77,7 @@ def start():
     cats = cases_cats()
     imgs = images_to_cats()
     # ---
-    new = { x: cats[v] for x, v in imgs if v in cats }
+    new = { x: cats[v] for x, v in imgs.items() if v in cats }
     # ---
     print(f"{len(new)=}")
     for numb, (file, cat) in enumerate(new.items(), start=1):
