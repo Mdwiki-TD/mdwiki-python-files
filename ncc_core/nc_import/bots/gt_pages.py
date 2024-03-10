6b61c2632ee3717bac734a0712459c9f45eb6e72
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-03-10 04:37:01 +0300

    up

--- ncc_core/nc_import/bots/gt_pages.py
+++ ncc_core/nc_import/bots/gt_pages.py
@@ -1,9 +1,9 @@
-
 from newapi.wiki_page import MainPage, NEW_API
+
 # api_new = NEW_API('en', family='wikipedia')
 # login    = api_new.Login_to_wiki()
 # pages  = api_new.Get_template_pages(title, namespace="*", Max=10000)
-'''
+"""
 page      = MainPage(title, 'ar', family='wikipedia')
 exists    = page.exists()
 text      = page.get_text()
@@ -15,14 +15,14 @@ purge     = page.purge()
 templates = page.get_templates()
 save_page = page.save(newtext='', summary='', nocreate=1, minor='')
 create    = page.Create(text='', summary='')
-'''
+"""
+
 
 def get_pages(code):
-    api_new = NEW_API(code, family='wikipedia')
+    api_new = NEW_API(code, family="wikipedia")
 
     api_new.Login_to_wiki()
-    
+
     pages = api_new.Get_template_pages("Template:NC", namespace="*", Max=10000)
 
     return pages
-
--- ncc_core/nc_import/bots/import_files.py
+++ ncc_core/nc_import/bots/import_files.py
@@ -1,14 +1,36 @@
+"""
 
+bot for importing files from nccommons to wikipedia
 
-from newapi.wiki_page import MainPage
+"""
+import sys
+# ---
+# from newapi.wiki_page import MainPage, NEW_API
+from newapi.ncc_page import MainPage as ncc_MainPage, NEW_API as ncc_NEW_API
 from newapi import printe
-'''
-page      = MainPage(title, 'ar', family='wikipedia')
-exists    = page.exists()
-text      = page.get_text()
-save_page = page.save(newtext='', summary='', nocreate=1, minor='')
-'''
+# ---
+from nc_import.bots import upload_file
+# upload = upload_file.upload_by_url(file_name, text, url, comment='', code="en", family="wikipedia")
+
+def get_file_text(title):
+    title = f"File:{title}" if not title.startswith("File:") else title
+    printe.output(f"<<yellow>>get_file_text: {title} from nccommons:")
+
+    page = ncc_MainPage(title, "www", family="nccommons")
+    text = page.get_text()
+
+    return text
+
 
 def import_file(title, code):
     printe.output(f"<<yellow>>import_file: File:{title} to {code}wiki:")
-    
+    # ---
+    file_text = get_file_text(title)
+    # ---
+    api_new  = ncc_NEW_API('www', family='nccommons')
+    # api_new.Login_to_wiki()
+    img_url = api_new.Get_image_url(title)
+    # ---
+    upload = upload_file.upload_by_url(title, file_text, img_url, comment='Bot: import from nccommons.org', code=code, family="wikipedia")
+    # ---
+    return upload
