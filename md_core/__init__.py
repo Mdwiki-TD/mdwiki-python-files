0a172fd2d43b2868ccf59c12cddaadc1cd7a5a5b
ibrahem <ibrahem.al-radaei@outlook.com>
2024-03-26 02:31:28 +0300

    u

--- md_core/__init__.py
+++ md_core/__init__.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""The initialization file for the Pywikibot framework."""
-#
-# (C) Pywikibot team, 2008-2017
-#
-# Distributed under the terms of the MIT license.
-#
 # find      :   \s*\n(\s*#\s*[-]+\s*)+\n*^def
 # find      :   \s*\n(#\s*[-]+\s*)+\n*def
 # replace   :   \n\n\ndef
