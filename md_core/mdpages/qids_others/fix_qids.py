89b2b04fce72123e37dd9ec501a6ef5d2f11549a
ibrahem <ibrahem.al-radaei@outlook.com>
2024-03-26 04:53:32 +0300

    1

--- md_core/mdpages/qids_others/fix_qids.py
+++ md_core/mdpages/qids_others/fix_qids.py
@@ -5,29 +5,16 @@ python3 core8/pwb.py qids_others/qids_others/fix_qids
 
 """
 import sys
+from mdpy.bots import catdepth2
 from mdpy.bots import wikidataapi
 from mdpy import printe
+from mdpy.bots.check_title import valid_title
 from mdpy.bots import sql_for_mdwiki
 from mdpy.bots import sql_qids_others
 from unlinked_wb.bot import work_un
-# ---
-qids_othrs = sql_qids_others.get_others_qids()
-# ---
-qids_to_title = {q: t for t, q in qids_othrs.items()}
-# ---
-to_work = [q for q in qids_othrs.values() if q != '']
 
 
-def fix_redirects():
-    # ---
-    printe.output('<<lightyellow>> start fix_redirects()')
-    # ---
-    new_list = to_work
-    # ---
-    reds = wikidataapi.get_redirects(new_list)
-    # ---
-    printe.output(f'len of redirects: {len(reds)}')
-    # ---
+def replace_in_sql(reds):
     for numb, (old_q, new_q) in enumerate(reds.items(), start=1):
         # ---
         printe.output(f'<<lightblue>> {numb}, old_q: {old_q}, new_q: {new_q}')
@@ -40,11 +27,40 @@ def fix_redirects():
         else:
             printe.output(qua)
             printe.output('<<lightgreen>> add "fix" to sys.argv to fix them..')
+
+
+def fix_redirects(to_work):
+    # ---
+    printe.output('<<lightyellow>> start fix_redirects()')
+    # ---
+    new_list = list(to_work.keys())
+    # ---
+    reds = wikidataapi.get_redirects(new_list)
+    # ---
+    printe.output(f'len of redirects: {len(reds)}')
+    # ---
+    if reds:
+        printe.output('<<lightgreen>> add "fix" to sys.argv to fix them..')
+        # ---
+        replace_in_sql(reds)
     # ----
-    tab = {qids_to_title.get(old_q) : new_q for old_q, new_q in reds.items() if qids_to_title.get(old_q) }
+    tab = {to_work.get(old_q) : new_q for old_q, new_q in reds.items() if to_work.get(old_q) }
     # ----
     work_un(tab)
 
 
+def start():
+    # ---
+    sql_qids = sql_qids_others.get_others_qids()
+    # ---
+    to_work = {q: t for t, q in sql_qids.items() if q != ''}
+    # ---
+    fix_redirects(to_work)
+    print('_______________d')
+    print('_______________d')
+    print('_______________d')
+    # add_to_qids(sql_qids)
+
+
 if __name__ == '__main__':
-    fix_redirects()
+    start()
