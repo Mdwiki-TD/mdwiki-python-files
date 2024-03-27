0f9e996002d1a2a266ae1b0b93887276adf346db
ibrahem <ibrahem.al-radaei@outlook.com>
2024-03-27 03:29:31 +0300

    Update findqids.sh

--- md_core/mdpages/findqids.sh
+++ md_core/mdpages/findqids.sh
@@ -2,11 +2,7 @@
 
 $HOME/local/bin/python3 core8/pwb.py td_other_qids/make_list td add
 
-$HOME/local/bin/python3 core8/pwb.py td_other_qids/make_list other add
-
 $HOME/local/bin/python3 core8/pwb.py mdpages/get_md_to_en
-
-$HOME/local/bin/python3 core8/pwb.py mdpages/get_md_to_en other
 
 $HOME/local/bin/python3 core8/pwb.py mdpages/find_qids redirects add
 
@@ -16,6 +12,13 @@ $HOME/local/bin/python3 core8/pwb.py mdpages/get_red
 
 $HOME/local/bin/python3 core8/pwb.py p11143_bot/bot -td add addq
 
-$HOME/local/bin/python3 core8/pwb.py p11143_bot/bot -others add addq
-
 $HOME/local/bin/python3 core8/pwb.py unlinked_wb/bot
+
+
+# other
+
+$HOME/local/bin/python3 core8/pwb.py td_other_qids/make_list other add
+
+$HOME/local/bin/python3 core8/pwb.py mdpages/get_md_to_en other
+
+$HOME/local/bin/python3 core8/pwb.py p11143_bot/bot -others add addq
