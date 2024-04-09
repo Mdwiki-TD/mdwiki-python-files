e29c86340beeaf1da73c8e330a2aac36ff8dec3f
ibrahem <ibrahem.al-radaei@outlook.com>
2024-04-10 01:46:27 +0300

    1

--- ncc_core/nccommons/fix_svg.py
+++ ncc_core/nccommons/fix_svg.py
@@ -9,21 +9,6 @@ from tempfile import mkstemp
 from shutil import move, copymode
 from os import fdopen, remove
 
-def remove_svg_dtd2(file_path, url=""):
-    """
-    function to remove dtd tags like: <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.0//EN'  'http://www.w3.org/Graphics/SVG/1.0/DTD/svg10.dtd'>
-    Args:
-        file_path (str): The path to the SVG file.
-        url (str, optional): The URL of the SVG file if accessed remotely. Defaults to "".
-
-    Returns:
-        str: file_path
-    """
-    # ---
-    if not file_path and url:
-        file_path, _ = urllib.request.urlretrieve(url)
-    # ---
-
 def remove_svg_dtd(file_path, url=""):
     """
     function to remove dtd tags like: <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.0//EN'  'http://www.w3.org/Graphics/SVG/1.0/DTD/svg10.dtd'>
