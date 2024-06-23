ce80ad686e1dde7e3769e7f863f14fac08143a7d
ibrahem <ibrahem.al-radaei@outlook.com>
2024-06-24 02:45:11 +0300

    .

--- mass/eyerounds/eyerounds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$HOME/local/bin/python3 core8/pwb.py mass/eyerounds/geturls
-
-$HOME/local/bin/python3 core8/pwb.py mass/eyerounds/getimages
-
-$HOME/local/bin/python3 core8/pwb.py mass/eyerounds/all_cats
-
-$HOME/local/bin/python3 core8/pwb.py mass/eyerounds/up
