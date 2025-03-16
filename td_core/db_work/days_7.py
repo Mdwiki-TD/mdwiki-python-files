e947e672465518fd8a4326080a2db5952474e6f0
ibrahem <ibrahem.al-radaei@outlook.com>
2025-03-16 04:26:13 +0300

    Update days_7.py

--- td_core/db_work/days_7.py
+++ td_core/db_work/days_7.py
@@ -8,12 +8,12 @@ from mdapi_sql import sql_for_mdwiki
 
 # sql_for_mdwiki.mdwiki_sql(query , update = False)
 # ---
-quaries = {
+queries = {
     "pages": "where date < DATE_SUB(current_timestamp(), INTERVAL 7 DAY) and (target = '' OR target IS NULL)",
     "in_process": "where add_date < DATE_SUB(current_timestamp(), INTERVAL 7 DAY) ",
 }
 # ---
-for name, qua in quaries.items():
+for name, qua in queries.items():
     print(f"--- {name} ---")
     # ---
     qua_select = f"select * from {name} {qua}"
@@ -23,7 +23,7 @@ for name, qua in quaries.items():
     for n, page in enumerate(pages, start=1):
         print(name, n, page)
     # ---
-    qua_del = f" delete from {name} {qua}"
+    qua_del = f"delete from {name} {qua}"
     # ---
     print(qua_del)
     # ---
