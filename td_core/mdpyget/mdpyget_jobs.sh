27a588c859d53e94d4af886dc8a91ae01b842214
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-08 01:07:33 +0300

    Update mdpyget_jobs.sh

--- td_core/mdpyget/mdpyget_jobs.sh
+++ td_core/mdpyget/mdpyget_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$HOME/local/bin/python3 core8/pwb.py mdpyget/sqlviews
+# $HOME/local/bin/python3 core8/pwb.py mdpyget/sqlviews
 $HOME/local/bin/python3 core8/pwb.py mdpyget/sqlviews_new
 
 $HOME/local/bin/python3 core8/pwb.py mdpyget/getas
