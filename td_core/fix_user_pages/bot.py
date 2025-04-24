6cb188303dfb069c02e9fb18d5cf5d1ee72aff63
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-25 02:45:23 +0300

    u

--- td_core/fix_user_pages/bot.py
+++ td_core/fix_user_pages/bot.py
@@ -15,8 +15,9 @@ import tqdm
 from newapi import printe
 from mdapi_sql import sql_for_mdwiki
 from db_work.check_titles_helps import get_new_target_log, Find_pages_exists, WikiPage
-from fix_user_pages.fix_it_db import work_in_new_tabs_to_db
+# from fix_user_pages.fix_it_db import work_in_new_tabs_to_db
 from fix_user_pages.fix_it_db_new import work_in_new_tabs_to_db_new
+from fix_user_pages.user_bot import get_new_user
 
 skip_by_lang = {
     "ar": ["الأنسولين"],
@@ -145,94 +146,6 @@ def work_in_titles(lang, tabs):
     # work_in_new_tabs_to_db(new_tabs_to_db)
     # ---
     work_in_new_tabs_to_db_new(toto)
-
-
-def count_users(revisions):
-    tab = {}
-    # ---
-    for x in revisions:
-        user = x.get("user")
-        # ---
-        tab.setdefault(user, 0)
-        # ---
-        tab[user] += 1
-    # ---
-    # sort tab
-    tab = dict(sorted(tab.items(), key=lambda x: x[1], reverse=True))
-    # ---
-    return tab
-
-
-def get_new_user(new_target, lang, user):
-    # ---
-    page = WikiPage(new_target, lang, family="wikipedia")
-    # ---
-    # user = page.get_user()
-    # userinfo    = page.get_userinfo() # "id", "name", "groups"
-    revisions = page.get_revisions(rvprops=[])
-    # ---
-    second_revid = 0
-    # ---
-    for x in revisions:
-        x_user = x.get("user")
-        if "parentid" in x and x["parentid"] == 0:
-            # ---
-            second_revid = x["revid"]
-            # ---
-            if x_user != user:
-                printe.output(f"<<red>> user:{user} new page not new!, created by :{x_user}")
-                return False
-            else:
-                printe.output(f"<<green>> new page created by :{x_user}")
-            # ---
-            break
-    # ---
-    false_users = ["Annacecilia2", "Doc James"]
-    # ---
-    if user in false_users:
-        # ---
-        revs = [x for x in revisions if x["parentid"] == second_revid]
-        # ---
-        if revs and revs[0]["user"] not in false_users:
-            # ---
-            rev_user = revs[0]["user"]
-            # ---
-            in_db = rev_user in db_users
-            # ---
-            printe.output(f"<<green>> rev_user is :{rev_user}, {in_db=}")
-            # ---
-            if in_db:
-                return rev_user
-        # ---
-        user_2nd = ""
-        # ---
-        users_count = count_users(revisions)
-        # ---
-        for x in revisions:
-            # ---
-            x_user = x.get("user")
-            # ---
-            x_count = users_count.get(x_user, 0)
-            # ---
-            if x_user not in false_users:
-                # ---
-                if not user_2nd:
-                    user_2nd = x_user
-                # ---
-                in_db = x_user in db_users
-                # ---
-                printe.output(f"<<green>> x_user is: {x_user}, {in_db=}, {x_count=}")
-                # ---
-                if not in_db and x_count == 1:
-                    continue
-                # ---
-                return x_user
-        # ---
-        return user_2nd
-    # ---
-    # print(f"get_new_user: {user=}, {revisions=}")
-    # ---
-    return ""
 
 
 def work_in_to_set(new_target, tab):
---
+++ td_core/fix_user_pages/user_bot.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""
+from fix_user_pages.user_bot import get_new_user
+"""
+from newapi import printe
+from mdapi_sql import sql_for_mdwiki
+from db_work.check_titles_helps import WikiPage
+
+db_users = sql_for_mdwiki.get_db_users()
+
+def count_users(revisions):
+    tab = {}
+    # ---
+    for x in revisions:
+        user = x.get("user")
+        # ---
+        tab.setdefault(user, 0)
+        # ---
+        tab[user] += 1
+    # ---
+    # sort tab
+    tab = dict(sorted(tab.items(), key=lambda x: x[1], reverse=True))
+    # ---
+    return tab
+
+
+def get_new_user(new_target, lang, user):
+    # ---
+    page = WikiPage(new_target, lang, family="wikipedia")
+    # ---
+    # user = page.get_user()
+    # userinfo    = page.get_userinfo() # "id", "name", "groups"
+    revisions = page.get_revisions(rvprops=[])
+    # ---
+    second_revid = 0
+    # ---
+    for x in revisions:
+        x_user = x.get("user")
+        if "parentid" in x and x["parentid"] == 0:
+            # ---
+            second_revid = x["revid"]
+            # ---
+            if x_user != user:
+                printe.output(f"<<red>> user:{user} new page not new!, created by :{x_user}")
+                return False
+            else:
+                printe.output(f"<<green>> new page created by :{x_user}")
+            # ---
+            break
+    # ---
+    false_users = ["Annacecilia2", "Doc James"]
+    # ---
+    if user in false_users:
+        # ---
+        revs = [x for x in revisions if x["parentid"] == second_revid]
+        # ---
+        if revs and revs[0]["user"] not in false_users:
+            # ---
+            rev_user = revs[0]["user"]
+            # ---
+            in_db = rev_user in db_users
+            # ---
+            printe.output(f"<<green>> rev_user is :{rev_user}, {in_db=}")
+            # ---
+            if in_db:
+                return rev_user
+        # ---
+        user_2nd = ""
+        # ---
+        users_count = count_users(revisions)
+        # ---
+        for x in revisions:
+            # ---
+            x_user = x.get("user")
+            # ---
+            x_count = users_count.get(x_user, 0)
+            # ---
+            if x_user not in false_users:
+                # ---
+                if not user_2nd:
+                    user_2nd = x_user
+                # ---
+                in_db = x_user in db_users
+                # ---
+                printe.output(f"<<green>> x_user is: {x_user}, {in_db=}, {x_count=}")
+                # ---
+                if not in_db and x_count == 1:
+                    continue
+                # ---
+                return x_user
+        # ---
+        return user_2nd
+    # ---
+    # print(f"get_new_user: {user=}, {revisions=}")
+    # ---
+    return ""
