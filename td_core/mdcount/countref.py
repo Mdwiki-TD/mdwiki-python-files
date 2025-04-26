4a350da529829088a5b4432507b30c40d778b7a4
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-26 03:48:28 +0300

    Refactor countref.py to improve data handling and streamline reference counting logic

--- td_core/mdcount/countref.py
+++ td_core/mdcount/countref.py
@@ -18,8 +18,6 @@ python3 core8/pwb.py mdcount/countref -title:Esophageal_rupture
 import os
 import json
 import sys
-from pathlib import Path
-
 
 from mdapi_sql import sql_for_mdwiki
 from apis import mdwiki_api
@@ -27,39 +25,44 @@ from newapi import printe
 from mdcount.bots.regex_scanner import RegexScanner
 from mdcount.bots.links import get_links_from_cats
 from mdpyget.bots.to_sql import to_sql
-
+# ---
 if os.getenv("HOME"):
     Dashboard_path = os.getenv("HOME") + "/public_html/td"
 else:
     Dashboard_path = "I:/mdwiki/mdwiki/public_html/td"
 # ---
-all_ref = {}
-lead_ref = {}
+all_tab_ref = {1: {}}
+lead_tab_ref = {1: {}}
+# ---
 vaild_links = {1: []}
 # ---
 file_all = f"{Dashboard_path}/Tables/jsons/all_refcount.json"
 file_lead = f"{Dashboard_path}/Tables/jsons/lead_refcount.json"
-# ---
-a = {}
-# ---
-with open(file_all, "r", encoding="utf-8") as f:
-    a = json.load(f)
-# ---
-all_ref = {x: ref for x, ref in a.items() if ref > 0}
-# ---
-la = {}
-# ---
-with open(file_lead, "r", encoding="utf-8") as f:
-    la = json.load(f)
-# ---
-lead_ref = {x: ref for x, ref in la.items() if ref > 0}
-# ---
-# list for titles in both all_ref and lead_ref
-list_fu = list(set(all_ref.keys()) & set(lead_ref.keys()))
-# ---
-# remove duplicates from list
-list_fu = list(set(list_fu))
-list_ma = {1: [x for x in list_fu if (x in all_ref and x in lead_ref)]}
+
+
+def get_jsons():
+    tab_all = {}
+    # ---
+    with open(file_all, "r", encoding="utf-8") as f:
+        tab_all = json.load(f)
+    # ---
+    tab_lead = {}
+    # ---
+    with open(file_lead, "r", encoding="utf-8") as f:
+        tab_lead = json.load(f)
+    # ---
+    all_ref = {x: ref for x, ref in tab_all.items() if ref > 0}
+    lead_ref = {x: ref for x, ref in tab_lead.items() if ref > 0}
+    # ---
+    que = "select DISTINCT r_title, r_lead_refs, r_all_refs from refs_counts"
+    # ---
+    in_sql = sql_for_mdwiki.mdwiki_sql_dict(que)
+    # ---
+    all_ref.update({x["r_title"]: x["r_all_refs"] for x in in_sql if x["r_all_refs"] > 0 and x["r_title"] not in all_ref})
+    lead_ref.update({x["r_title"]: x["r_lead_refs"] for x in in_sql if x["r_lead_refs"] > 0 and x["r_title"] not in lead_ref})
+    # ---
+    all_tab_ref[1] = all_ref
+    lead_tab_ref[1] = lead_ref
 
 
 def get_refs_new(text):
@@ -121,12 +124,12 @@ def count_refs(title):
     # text2 = ref.fix_ref(text, text)
     # ---
     all_c = count_ref_from_text(text2)
-    all_ref[title] = all_c
+    all_tab_ref[1][title] = all_c
     # ---
     leadtext = text2.split("==")[0]
     lead_c = count_ref_from_text(leadtext, get_short=True)
     # ---
-    lead_ref[title] = lead_c
+    lead_tab_ref[1][title] = lead_c
     # ---
     printe.output(f"<<green>> all:{all_c} \t lead:{lead_c}")
 
@@ -138,36 +141,58 @@ def logaa(file, table):
     printe.output(f"<<green>> {len(table)} lines to {file}")
 
 
-def from_sql():
+def from_sql(old_values):
     # ---
-    que = """select title, word from pages;"""
+    que = """select title from titles_infos;"""
     # ---
     sq = sql_for_mdwiki.select_md_sql(que, return_dict=True)
     # ---
     titles2 = [q["title"] for q in sq]
     # ---
-    titles = [x for x in titles2 if x not in list_ma[1]]
+    titles = [x for x in titles2 if x not in old_values]
     # ---
     printe.output(f"<<yellow>> sql: find {len(titles2)} titles, {len(titles)} to work. ")
     return titles
+
+
+def make_old_values():
+    # list for titles in both all_tab_ref[1] and lead_tab_ref[1]
+    list_fu = list(set(all_tab_ref[1].keys()) & set(lead_tab_ref[1].keys()))
+    # ---
+    # remove duplicates from list
+    list_fu = list(set(list_fu))
+    # ---
+    list_ma = {1: [x for x in list_fu if (x in all_tab_ref[1] and (x in lead_tab_ref[1] or x.lower().startswith("video:")))]}
+    # ---
+    return list_ma
 
 
 def get_links():
     # ---
     titles = []
     # ---
+    old_values = make_old_values()
+    # ---
     if "sql" in sys.argv:
-        titles = from_sql()
+        titles = from_sql(old_values)
     else:
         titles = get_links_from_cats()
     # ---
     if "newpages" in sys.argv:
-        titles = [x for x in titles if (x not in list_ma[1])]
+        titles = [x for x in titles if (x not in old_values)]
     # ---
     return titles
 
 
+def start_to_sql():
+    data2 = [{"r_title": x, "r_lead_refs": v, "r_all_refs": all_tab_ref[1].get(x, 0)} for x, v in lead_tab_ref[1].items()]
+    # ---
+    to_sql(data2, "refs_counts", ["r_title", "r_lead_refs", "r_all_refs"], title_column="r_title")
+
+
 def mai():
+    # ---
+    get_jsons()
     # ---
     numb = 0
     # ---
@@ -196,30 +221,24 @@ def mai():
         count_refs(x)
         # ---
         if numb == 10 or str(numb).endswith("00"):
-            logaa(file_lead, lead_ref)
-            logaa(file_all, all_ref)
+            logaa(file_lead, lead_tab_ref[1])
+            logaa(file_all, all_tab_ref[1])
         # ---
     # ---
-    logaa(file_lead, lead_ref)
-    logaa(file_all, all_ref)
+    logaa(file_lead, lead_tab_ref[1])
+    logaa(file_all, all_tab_ref[1])
     # ---
     start_to_sql()
-
-
-def start_to_sql():
-    data2 = [{"r_title": x, "r_lead_refs": v, "r_all_refs": all_ref.get(x, 0)} for x, v in lead_ref.items()]
-    # ---
-    to_sql(data2, "refs_counts", ["r_title", "r_lead_refs", "r_all_refs"], title_column="r_title")
 
 
 def test():
     # python3 core8/pwb.py mdcount/countref test
     # ---
-    lead_ref["Yemen1"] = 50
-    all_ref["Yemen1"] = 50
+    lead_tab_ref[1]["Yemen1"] = 50
+    all_tab_ref[1]["Yemen1"] = 50
     # ---
-    lead_ref["Sana'a"] = 500
-    all_ref["Sana'a"] = 100
+    lead_tab_ref[1]["Sana'a"] = 500
+    all_tab_ref[1]["Sana'a"] = 100
     # ---
     start_to_sql()
 
