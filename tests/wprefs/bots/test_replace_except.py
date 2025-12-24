0c22d4744975e6c709287483318bc6f5e6f2a635
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-12-24 05:38:40 +0300

    1

--- tests/wprefs/bots/test_replace_except.py
+++ tests/wprefs/bots/test_replace_except.py
@@ -1,7 +1,6 @@
 
 import pytest
 
-from wprefs.bots.replace_except import replaceExcept, removeprefix
-from wprefs.bots.replace_except_new import replaceExcept as replaceExceptNew, removeprefix as removeprefixNew
+from wprefs.bots.replace_except import replaceExcept
 
-# TODO: write tests to compare the outputs of replaceExcept and replaceExceptNew functions
+# TODO: write tests for replaceExcept
---
+++ tests/wprefs/bots/test_replace_except_compare.py
@@ -0,0 +1,7 @@
+
+import pytest
+
+from wprefs.bots.replace_except import replaceExcept
+from wprefs.bots.replace_except_new import replaceExcept as replaceExceptNew
+
+# TODO: write tests to compare the outputs of replaceExcept and replaceExceptNew functions
