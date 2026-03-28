c3827af816e89bc25f12214a3505accdedbdacde
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-28 05:03:52 +0300

    .

--- md_core_helps/apis/user_accounts.py
+++ md_core_helps/apis/user_accounts.py
@@ -9,11 +9,8 @@ try:
 except Exception:
     pass
 
-username = os.getenv("WIKIPEDIA_BOT_USERNAME")
-password = os.getenv("WIKIPEDIA_BOT_PASSWORD")
-
-bot_username = username
-bot_password = password
+bot_username = os.getenv("WIKIPEDIA_BOT_USERNAME")
+bot_password = os.getenv("WIKIPEDIA_BOT_PASSWORD")
 
 my_username = os.getenv("WIKIPEDIA_HIMO_USERNAME")
 mdwiki_pass = os.getenv("MDWIKI_HIMO_PASSWORD")
