507ae527fa79eacbf7b5b280db8d6d74e4e50404
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-12 06:10:36 +0300

    .

--- tests/copy_to_en/test_alltext_changes.py
+++ tests/copy_to_en/test_alltext_changes.py
@@ -11,7 +11,7 @@ from src.copy_to_en.bots import alltext_changes
 
 class TestAllTextChanges:
     def setup_method(self):
-        self.test_file = Path(__file__).parent / "sample_text.txt"
+        self.test_file = Path(__file__).parent / "sample_text.wiki"
         with open(self.test_file, "r", encoding="utf-8") as f:
             self.test_text = f.read()
 
